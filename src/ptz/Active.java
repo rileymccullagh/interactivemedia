ad02e854d54b35596f8045d184d7d39d4977cfdc
Ben Krajancic <Fantasmos@users.noreply.github.com>
2018-09-30 07:50:50 +1000

    Async retrieval of multiple images
    needs to be expanded upon however, we need to have an image retrieval management system to ensure we don't spam too much at one time. I'm thinking a recursive function rn.

--- src/ptz/Active.java
+++ src/ptz/Active.java
@@ -1,34 +1,41 @@
 package ptz;
 
+import barballview.Engine_Ball_Bar;
+//import drummachine.DrumMachine;
 import processing.core.PApplet;
 import webcam.Camera;
-import drummachine.DrumMachine;
-import barballview.Engine_Ball_Bar;
 
 class Active {
 	PApplet parent;
 	FFT fft;
 	Engine_Ball_Bar engine;
 	Camera cam;
-
-	DrumMachine dm;
+	boolean retrieving = false;
+	//DrumMachine dm;
 	int lastTrigger = 0;
 
 	Active(PApplet p) {
 		this.parent = p;
 		this.fft = new FFT(this.parent);
 		this.engine = new Engine_Ball_Bar(p.width / 3, p.height / 3, fft.values.length, p);
-		this.dm = new DrumMachine(this.parent);
+		//this.dm = new DrumMachine(this.parent);
 		this.cam = new Camera(this.parent);
+		
+		//cam.get_amount_of_images(30);
 	}
 
 	void draw() {
+		parent.loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Logo_apple_pnh.png/493px-Logo_apple_pnh.png");
+		if (retrieving == false) {
+			cam.get_amount_of_images(6); 
+			retrieving = true;
+		}
 		parent.clear();
  		//parent.background(51, 51, 126);
 		fft.update();
 
 		if(parent.millis() > lastTrigger +5000) {
-			dm.trigger(1);
+			//dm.trigger(1);
 			lastTrigger = parent.millis();
 		}
 
--- src/webcam/Camera.java
+++ src/webcam/Camera.java
@@ -1,5 +1,9 @@
 package webcam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 // cameras from here work
 // https://www.insecam.org/en/bytype/Vivotek/
 
@@ -8,33 +12,80 @@ import processing.core.PImage;
 
 public class Camera {
   PApplet parent;
+  List<PImage> images = new ArrayList<PImage>();
+  PImage empty_image;
+  
+  int num_of_images = 0;
+  String pre_string = "http://85.187.13.43:8080/cgi-bin/viewer/video.jpg?r=";
+  int current_number = 1538256987;
+  int position = -1;
+  int latest_retrieved = 0;
+  
   PImage current, future;
   boolean newImage;
   boolean retrieving = false;
   public Camera(PApplet p) {
-    this.parent = p;
-    getAsync();
+	this.parent = p;
+	getAsync();
+	empty_image = p.createImage(0, 0, 0);
   }
-  int num = 0;
-  public PImage getNext() {
-	  //System.out.println("Getting");
-	  if (retrieving == false) {
-		  System.out.println("Enter");
-		  retrieving = true;
-		  new Thread (new Runnable() {
+  
+  public void update_image() {
+	  new Thread (new Runnable() {
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				num++;
-				current = parent.loadImage("http://85.187.13.43:8080/cgi-bin/viewer/video.jpg?r=153779841" + num);
-				retrieving = false;
+				position++;
+				current_number--;
+				images.add(position, parent.loadImage("http://85.187.13.43:8080/cgi-bin/viewer/video.jpg?r=" + Integer.toString(current_number)));
 				System.out.println("EXECUTED ASYNC");
 			}
+			}).start();
+  }
+  
+  public void get_amount_of_images(int amount) {
+	  for (int i = 0; i < amount; ++i) {
+		  ++position;
+		  current_number--;
+		  
+		  new Thread (new Runnable() {
+			  int position_to_insert = position; //Don't move this into run
+			  @Override
+				public void run() {
+				  try{
+					  images.add(empty_image);
+					  
+					  PImage new_image = parent.loadImage("http://85.187.13.43:8080/cgi-bin/viewer/video.jpg?r=" + Integer.toString(current_number));
+					  if (new_image == null) {
+						  images.remove(position_to_insert);
+					  }
+					  else {
+						  images.set(position_to_insert, new_image);
+					  }
+					  System.out.println(position_to_insert);
+				  } catch (Exception e) {
+					  System.out.println(position_to_insert);
+					  System.out.println(Integer.toString(current_number));
+				  }
+			  }
 		  }).start();
 	  }
-	  //this.current = parent.loadImage("http://85.187.13.43:8080/cgi-bin/viewer/video.jpg?r=1537798410");
-	  //parent.thread("getAsync");
-	  return current;
+  }
+  
+  public PImage getNext() {
+	  if (images.isEmpty()){
+		  return empty_image;
+	  } else {
+		  latest_retrieved %= images.size();
+		  if (images.get(latest_retrieved).height > 0) {
+			  if (images.size() != 1) {
+				  latest_retrieved++;
+			  }
+			  return images.get(latest_retrieved % images.size());
+		  } else {
+			  latest_retrieved %= images.size();
+			  return images.get(0);
+		  }
+	  }
   }
   
   public void getAsync() {
