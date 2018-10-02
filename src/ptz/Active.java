d40ca2673934cef49f098668c1980afa9dd37508
Ben <benji.k@live.com.au>
2018-10-03 07:59:26 +1000

    Updated active to demonstate interactive with camera better

--- src/ptz/Active.java
+++ src/ptz/Active.java
@@ -25,37 +25,28 @@ class Active {
 		this.cam = new Camera(this.parent);
 		
 		//cam.get_amount_of_images(30);
+		current_urls.add(cam.cameras.get(1));
+		current_urls.add(cam.cameras.get(2));
+		current_urls.add(cam.cameras.get(3));
+		current_urls.add(cam.cameras.get(4));
+		current_urls.add(cam.cameras.get(5));
+		current_urls.add(cam.cameras.get(6));
+		cam.download_multiple_images_in_sequence(current_urls, 3, 6); 
 	}
 
 	void draw() {
-		if (retrieving == false) {
-			//cam.download_multiple_images(cam.cameras.get(1), 30); 
-			
-			//current_urls.add(cam.cameras.get(0));
-			current_urls.add(cam.cameras.get(1));
-			current_urls.add(cam.cameras.get(2));
-			current_urls.add(cam.cameras.get(3));
-			current_urls.add(cam.cameras.get(4));
-			current_urls.add(cam.cameras.get(5));
-			current_urls.add(cam.cameras.get(6));
-			cam.download_multiple_images_in_sequence(current_urls, 10, 30); 
-			
-			retrieving = true;
-		}
+		//cam.cancel_threads(); //Later we can implement this, so we can prevent it retrieving during render
 		parent.clear();
- 		//parent.background(51, 51, 126);
 		fft.update();
 
 		if(parent.millis() > lastTrigger +5000) {
 			//dm.trigger(1);
 			lastTrigger = parent.millis();
 		}
-
-		int i = 0;
+		
 		int img_width = parent.width / current_urls.size();
 		int img_height = parent.height;
-		//parent.image(cam.getNextImage(cam.cameras.get(1)),0,0);
-		
+		int i = 0;
 		for (String entry : current_urls) {
 			parent.image(cam.getNextImage(entry),i * img_width, 0, img_width, img_height);
 			++i;
