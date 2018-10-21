8affcf2db8b597a30583566ac386ad9a17c42503
Ben <benji.k@live.com.au>
2018-10-22 10:26:52 +1100

    Added new interface classes

---
+++ src/ptz_camera/PC_Webcam.java
@@ -0,0 +1,22 @@
+package ptz_camera;
+
+import java.util.Optional;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.*;
+public class PC_Webcam implements GetImage {
+	String webcam_name;
+	
+	PC_Webcam(String webcam_name){
+		this.webcam_name = webcam_name;
+	}
+	@Override
+	public Optional<PImage> getNextImage(PApplet parent) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+
+}
